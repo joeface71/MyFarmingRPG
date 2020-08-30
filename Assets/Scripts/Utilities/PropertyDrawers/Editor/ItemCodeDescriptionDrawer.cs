67e195e7210293a9db8e4129042091d4b9b63bf2
joeface71 <joefacenda@yahoo.com>
2020-08-30 13:56:18 -0400

    Custom Property Attribute Drawer Added

---
+++ Assets/Scripts/Utilities/PropertyDrawers/Editor/ItemCodeDescriptionDrawer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ItemCodeDescriptionAttribute))]
+public class ItemCodeDescriptionDrawer : PropertyDrawer
+{
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        // Change returned property height to double for the additional line for the description
+        return EditorGUI.GetPropertyHeight(property) * 2;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // BeginProperty EndProperty on the parent property ensures that the prefab override logic works on the entire property
+        EditorGUI.BeginProperty(position, label, property);
+
+        if (property.propertyType == SerializedPropertyType.Integer)
+        {
+
+            EditorGUI.BeginChangeCheck(); // Starts check for changed values
+
+            // Draw Item Code
+            var newValue = EditorGUI.IntField(new Rect(position.x, position.y, position.width, position.height / 2), label, property.intValue);
+
+            // Draw Item Description
+            EditorGUI.LabelField(new Rect(position.x, position.y + position.height / 2, position.width, position.height / 2), "Item Description", GetItemDescription(property.intValue));
+
+
+
+            // If item code value has changed, then set value to new value
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.intValue = newValue;
+            }
+
+        }
+
+        EditorGUI.EndProperty();
+    }
+
+    private string GetItemDescription(int itemCode)
+    {
+        SOItemList soITemList;
+
+        soITemList = AssetDatabase.LoadAssetAtPath("Assets/Scriptable Object Assets/item/so_ItemList.asset", typeof(SOItemList)) as SOItemList;
+
+        List<ItemDetails> itemDetailsList = soITemList.itemDetails;
+
+        ItemDetails itemDetail = itemDetailsList.Find(x => x.itemCode == itemCode);
+
+        if (itemDetail != null)
+        {
+            return itemDetail.itemDescription;
+        }
+        else
+        {
+            return "";
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/PropertyDrawers/ItemCodeDescriptionAttribute.cs
@@ -0,0 +1,8 @@
+﻿
+using UnityEngine;
+
+public class ItemCodeDescriptionAttribute : PropertyAttribute
+{
+    // No values needed for item code description attribute
+    // Class can be empty
+}
